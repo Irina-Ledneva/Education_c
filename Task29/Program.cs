b97484b97e0d0f9cf057f2ea8ab320f667a6ddfa
Irina-Ledneva <111776703+Irina-Ledneva@users.noreply.github.com>
2023-03-22 23:51:33 +0200

    Added task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29
+// Напишите программу, которая 
+// 1. задаёт массив из 8 элементов и 
+// 2. выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arraySize];
+
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arraySize)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arraySize.Length; i++)
+    {
+        arraySize[i] = rnd.Next();
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
