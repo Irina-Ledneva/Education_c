c76319f5cfbb5d741b59c56cbca50555aa695466
Irina-Ledneva <111776703+Irina-Ledneva@users.noreply.github.com>
2023-03-22 22:58:30 +0200

    modified task 28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -5,18 +5,45 @@
 // 4 -> 24
 // 5 -> 120
 
+// Console.WriteLine("Введите целое положительное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int multiplication = Multiplication(number);
+// Console.WriteLine($"Произведение чисел от 1 до {number} = {multiplication}");
+
+// int Multiplication(int num)
+// {
+//     int mult = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         mult = mult * i;
+//     }
+//     return mult;
+// }
+
+
 Console.WriteLine("Введите целое положительное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int multiplication = Multiplication(number);
-Console.WriteLine($"Произведение чисел от 1 до {number} = {multiplication}");
-
-int Multiplication(int num)
+if (number > 0)
 {
-    int mult = 1;
+    int factorialNumbers = FactorialNumbers(number); //7
+    Console.WriteLine($"Факториал чисел от 1 до {number} = {factorialNumbers}");
+}
+else
+{
+    Console.WriteLine("Введите число больще нуля");
+}
+
+int FactorialNumbers(int num) // 7
+{
+    int multi = 1;
     for (int i = 1; i <= num; i++)
     {
-        mult = mult * i;
+        checked
+        {
+            multi -= i;
+        }
     }
-    return mult;
+    return multi;
 }
