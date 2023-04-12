983ccf0ed1aec61f49bed27fd9ab3693083ea7c0
Irina-Ledneva <111776703+Irina-Ledneva@users.noreply.github.com>
2023-04-13 02:36:14 +0300

    modified.Task66.Homework

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -5,16 +5,30 @@
 // M = 1; N = 15-> 120
 // M = 4; N = 8. -> 30
 
-
-Console.Write("Введите значение M: ");
+Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите значение N: ");
+
+Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int summa(int m, int n)
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
 {
-    if (m == n)
-        return n;
-    else return m + summa(m + 1, n);
+    Console.Write(SumMN(m - 1, n));
 }
-Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m, n)}");
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
 
