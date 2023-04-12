8e6fefe2e663a6bba39eb52c42b9c964b37af8d8
Irina-Ledneva <111776703+Irina-Ledneva@users.noreply.github.com>
2023-04-13 02:22:11 +0300

    modified.Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -5,15 +5,21 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите натуральное число: ");
+Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
 
-if (number > 0) NaturalNumbers(number);
-else Console.WriteLine("Неверное значение");
 
-void NaturalNumbers(int num)
+void NaturalToLow(int n, int count)
 {
-    if (num == 0) return;
-    Console.Write($"{num} ");
-    NaturalNumbers(num - 1);
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
 }
